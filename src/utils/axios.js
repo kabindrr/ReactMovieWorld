9c81e673d80186931434d43bff832c6110d7bf03
kabindrr <chaina_kabindra@yahoo.com>
2024-07-12 09:02:08 +1000

    test-2

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiKEY = "c2925841";
-const apiEP = `http://www.omdbapi.com/?apikey=${apiKEY}&`;
+const apiEP = `https://www.omdbapi.com/?apikey=${apiKEY}&`;
 // const apiEP = `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKEY}&`;
 export const fetchFromAPI = async (str) => {
   try {
